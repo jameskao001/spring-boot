abba53b64aad9a95617a2d30f59bf75954f66562
James Kao <jameskao@g.ncu.edu.tw>
2018-11-29 14:39:19 +0800

    add controller

---
+++ demo/src/main/java/com/example/demo/controller/SampleController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author James Kao on 2018/11/28
+ */
+@RestController
+public class SampleController {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello JK";
+    }
+}
