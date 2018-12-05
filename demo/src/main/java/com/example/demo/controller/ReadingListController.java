121441b3bc8e4baabe99364c055e00e36d95fa82
James Kao <jameskao@g.ncu.edu.tw>
2018-12-05 14:29:59 +0800

    demo project is worked

---
+++ demo/src/main/java/com/example/demo/controller/ReadingListController.java
@@ -0,0 +1,46 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Book;
+import com.example.demo.repository.ReadingListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+/**
+ * @author James Kao on 2018/12/4
+ */
+@Controller
+@RequestMapping("/readingList")
+public class ReadingListController {
+
+    private static final String reader = "craig";
+
+    private ReadingListRepository readingListRepository;
+
+    @Autowired
+    public ReadingListController(ReadingListRepository readingListRepository) {
+        this.readingListRepository = readingListRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String readersBooks(Model model) {
+
+        List<Book> readingList = readingListRepository.findByReader(reader);
+        if (readingList != null) {
+            model.addAttribute("books", readingList);
+        }
+        return "readingList";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String addToReadingList(Book book) {
+        book.setReader(reader);
+        readingListRepository.save(book);
+        return "redirect:/readingList";
+    }
+
+}
